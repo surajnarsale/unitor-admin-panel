234e3a0c9354a85f53525c394b621342cb52e4be
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-04 15:59:15 +0530

    feat: add logo and icon_badge

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,13 +6,17 @@ export default function Topbar() {
 	return (
 		<div className="topbar">
 			<div className="topbarWrapper">
-				<div className="topLeft"></div>
+				<div className="topLeft">
+					<span className="logo">UNITOR</span>
+				</div>
 				<div className="topRight">
 					<div className="topbarIconContainer">
 						<NotificationsNone />
+						<span className="topIconBadge">2</span>
 					</div>
 					<div className="topbarIconContainer">
 						<Language />
+						<span className="topIconBadge">2</span>
 					</div>
 					<div className="topbarIconContainer">
 						<Settings />
