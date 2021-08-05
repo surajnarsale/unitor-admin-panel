29bb8cf8e83213f0dfc9a5c2a61d6223784cdce8
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-05 16:21:17 +0530

    feat: add sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Topbar from './components/topbar/Topbar';
+import './app.css';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
 	return (
 		<div className="App">
 			<Topbar />
+			<Sidebar />
 		</div>
 	);
 }
