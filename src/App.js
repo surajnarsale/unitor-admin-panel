18b5d793be2fbf674b21cab2a7757b8fb20a9a78
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-06 18:14:18 +0530

    feat: add home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Topbar from './components/topbar/Topbar';
 import './app.css';
 import Sidebar from './components/sidebar/Sidebar';
+import Home from './pages/home/Home';
 
 function App() {
 	return (
 		<div className="App">
 			<Topbar />
-			<Sidebar />
+			<div className="container">
+				<Sidebar />
+				<Home />
+			</div>
 		</div>
 	);
 }
