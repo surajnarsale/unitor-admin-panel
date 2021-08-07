25e9006408b72c72fc1dababf3d61b920a9303fe
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-07 23:48:05 +0530

    feat: add chart to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './home.css';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import { userData } from '../../dummyData';
+import Chart from '../../components/chart/Chart';
 
 export default function Home() {
 	return (
 		<div className="home">
 			<FeaturedInfo />
+			<Chart
+				data={userData}
+				title="User Analytics"
+				grid
+				dataKey="Active User"
+			/>
 		</div>
 	);
 }
