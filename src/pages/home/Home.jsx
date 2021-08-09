6f45a0e18ebea0030a3295f342e6097ac5932a1a
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-09 06:52:36 +0530

    feat: add lgWidget to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import { userData } from '../../dummyData';
 import Chart from '../../components/chart/Chart';
 import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 
 export default function Home() {
 	return (
@@ -17,6 +18,7 @@ export default function Home() {
 			/>
 			<div className="homeWidgets">
 				<WidgetSm />
+				<WidgetLg />
 			</div>
 		</div>
 	);
