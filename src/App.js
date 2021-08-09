f46ac3f0369ba138ce2fb8a61eb9dec3d280c340
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-09 23:08:50 +0530

    feat: add routes to pages

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import Topbar from './components/topbar/Topbar';
 import './app.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
+		<Router>
 			<Topbar />
 			<div className="container">
 				<Sidebar />
-				<Home />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/users">
+						<UserList />
+					</Route>
+				</Switch>
 			</div>
-		</div>
+		</Router>
 	);
 }
 
