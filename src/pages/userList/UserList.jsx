bf396aa5f3882f2fa975bfb3b03db90410895d68
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-13 11:42:08 +0530

    feat: add user page routes

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -41,8 +41,9 @@ export default function UserList() {
 			renderCell: (params) => {
 				return (
 					<>
-						<button className="userListEdit">Edit</button>
-
+						<Link to={'/user/' + params.row.id}>
+							<button className="userListEdit">Edit</button>
+						</Link>
 						<DeleteOutline className="userListDelete" />
 					</>
 				);
