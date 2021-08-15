0fc9e90ffda27b9398ed78ee4f28ff10a943c5d8
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-15 23:38:26 +0530

    feat: add new user route

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -14,8 +14,9 @@ export default function User() {
 		<div className="user">
 			<div className="userTitleContainer">
 				<h1 className="userTitle">Edit User</h1>
-
-				<button className="userAddButton">Create</button>
+				<Link to="/newUser">
+					<button className="userAddButton">Create</button>
+				</Link>
 			</div>
 			<div className="userContainer">
 				<div className="userShow">
