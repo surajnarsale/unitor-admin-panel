2da0fc31f4cf5d40d28e3c093cbfd941ace18425
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-16 10:23:28 +0530

    feat: add product routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './pages/home/Home';
 import UserList from './pages/userList/UserList';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import User from './pages/user/User';
+import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
+import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
 
 function App() {
 	return (
@@ -21,6 +25,18 @@ function App() {
 					</Route>
 					<Route path="/user/:userId">
 						<User />
+					</Route>
+					<Route path="/newUser">
+						<NewUser />
+					</Route>
+					<Route path="/products">
+						<ProductList />
+					</Route>
+					<Route path="/products/:productsId">
+						<Product />
+					</Route>
+					<Route path="/newproduct">
+						<NewProduct />
 					</Route>
 				</Switch>
 			</div>
