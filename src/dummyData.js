4f9cab3bef52321010208c66c4618f6441ef060b
Suraj Narsale <surajsnarsale@gmail.com>
2021-08-19 08:47:37 +0530

    feat: add product-dummy-data

--- src/dummyData.js
+++ src/dummyData.js
@@ -224,3 +224,18 @@ export const productRows = [
 		price: '$120.00',
 	},
 ];
+
+export const productData = [
+	{
+		name: 'Jan',
+		Sales: 4000,
+	},
+	{
+		name: 'Feb',
+		Sales: 3000,
+	},
+	{
+		name: 'Mar',
+		Sales: 5000,
+	},
+];
